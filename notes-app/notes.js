bc0a263b10c1033555c30fb0efb3a089305e0e35
ccooper2382 <93221305+ccooper2382@users.noreply.github.com>
2022-12-27 14:50:27 -0600

    Add files via upload

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const chalk = require('chalk')
 
 const getNotes = function () {
     return 'Your notes...'
@@ -16,11 +17,42 @@ const addNote = function (title, body) {
             body: body
         })
         saveNotes(notes)
-        console.log('New note added!')
+        console.log(chalk.bgGreen('New note added!'))
     } else {
-        console.log('Please select a unique title')
+        console.log(chalk.bgRed('Please select a unique title'))
     }
 
+}
+
+const removeNote = function (title) {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter(function (note) {
+        return note.title !== title
+    })
+
+
+    if (notes.length > notesToKeep.length) {
+        console.log(chalk.bgGreen(`Note removed!`))
+        saveNotes(notesToKeep) // i need to do some research on how filter() works
+    } else {
+        console.log(chalk.bgRed("No note found!"))
+    }
+    //  my solution
+    // const deadNote = notes.filter(function (note) {
+    //     return note.title === title
+    // })
+    //
+    // if (deadNote.length !== 0) {
+    //     for (let i = 0; i < notes.length; i++ ) {
+    //         if (notes[i].title === title) {
+    //             let removed = notes.splice(i,1)
+    //             console.log(`Note with title ${removed.title} has been removed`)
+    //             saveNotes(notes)
+    //         }
+    //     }
+    // } else {
+    //     console.log(`No note with title of ${title} exists` )
+    // }
 }
 
 const saveNotes = function (notes) {
@@ -41,5 +73,6 @@ const loadNotes = function () {
 }
 module.exports = {
     getNotes: getNotes,
-    addNote: addNote
+    addNote: addNote,
+    removeNote: removeNote
 }
